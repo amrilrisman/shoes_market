bcf2ceec4fc2925b074fcad3575e74e0794cd45a
amrilrisman <amrilrisman@gmail.com>
2022-05-30 22:52:28 +0700

    add asset, and theme color and text

---
+++ lib/theme/theme.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+// color
+Color primaryColor = Color(0xFF6C5ECF);
+Color secondaryColor = Color(0xFF38ABBE);
+Color alertColor = Color(0xFFED6363);
+Color priceColor = Color(0xFF2C96F1);
+Color backgroundColor1 = Color(0xFF1F1D2B);
+Color backgroundColor2 = Color(0xFF2B2937);
+Color backgroundColor3 = Color(0xFF242231);
+Color primarTextColor = Color(0xFFE1E1E1);
+Color secondaryTextColor = Color(0xFF999999);
+
+// text style
+TextStyle primaryTextStyle = TextStyle(color: primarTextColor);
+TextStyle secondaryTextStyle = TextStyle(color: secondaryTextColor);
+TextStyle priceTextStyle = TextStyle(color: priceColor);
+
+// font weight
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
